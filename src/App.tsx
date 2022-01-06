d5a14827c9a8899b33313fcdaac9d78a855b1c50
programmergeek <programergeek@gmail.com>
2022-01-06 18:13:59 +0200

    create delete, clear all, edit and add icon components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import React from "react";
 import "./App.css";
-import { Icon } from "./Components";
-import { Android } from "@styled-icons/boxicons-logos/Android";
+import { ClearIcon } from "./Components";
 
 function App() {
   return (
     <div className="App">
-      <Icon>
-        <Android />
-      </Icon>
+      <ClearIcon />
     </div>
   );
 }
--- src/Components/Icons.tsx
+++ src/Components/Icons.tsx
@@ -1,5 +1,41 @@
 import React from "react";
+import { Add } from "@styled-icons/fluentui-system-filled/Add";
+import { Delete } from "@styled-icons/fluentui-system-filled/Delete";
+import { Edit } from "@styled-icons/fluentui-system-filled/Edit";
+import { ClearAll } from "@styled-icons/material-rounded/ClearAll";
 
 export const Icon: React.FC = ({ ...props }) => {
   return <div className="w-14">{props.children}</div>;
+};
+
+export const AddIcon: React.FC = () => {
+  return (
+    <Icon>
+      <Add />
+    </Icon>
+  );
+};
+
+export const DeleteIcon: React.FC = () => {
+  return (
+    <Icon>
+      <Delete />
+    </Icon>
+  );
+};
+
+export const EditIcon: React.FC = () => {
+  return (
+    <Icon>
+      <Edit />
+    </Icon>
+  );
+};
+
+export const ClearIcon: React.FC = () => {
+  return (
+    <Icon>
+      <ClearAll />
+    </Icon>
+  );
 };
