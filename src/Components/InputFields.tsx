df0d6c1022ddc895bfbcf461dfb1ba59c85d8a35
programmergeek <programergeek@gmail.com>
2022-01-11 16:01:26 +0200

    import Nunito font from google fonts

--- src/Components/InputFields.tsx
+++ src/Components/InputFields.tsx
@@ -5,7 +5,7 @@ export const TextField: React.FC = () => {
     <div>
       <input
         type="text"
-        className="text-field px-5 lg:w-2/5 md:w-1/2 w-4/5 rounded-full h-12 outline-none shadow-lg"
+        className="text-field px-5 lg:w-2/5 font-sans-serif md:w-1/2 w-4/5 rounded-full h-12 outline-none shadow-lg"
       />
     </div>
   );
