b09faca9663e5fb29b1c1b19a3b03f7343fe65f5
programmergeek <programergeek@gmail.com>
2022-01-11 16:05:39 +0200

    define props for text field components

--- src/Components/InputFields.tsx
+++ src/Components/InputFields.tsx
@@ -1,16 +1,22 @@
 import React from "react";
 
-export const TextField: React.FC = () => {
+type TextFieldProps = React.DetailedHTMLProps<
+  React.InputHTMLAttributes<HTMLInputElement>,
+  HTMLInputElement
+>;
+
+export const TextField: React.FC<TextFieldProps> = ({ ...props }) => {
   return (
     <div>
       <input
         type="text"
         className="text-field px-5 lg:w-2/5 font-sans-serif md:w-1/2 w-4/5 rounded-full h-12 outline-none shadow-lg"
+        {...props}
       />
     </div>
   );
 };
 
-export const InlineTextField: React.FC = () => {
+export const InlineTextField: React.FC<TextFieldProps> = ({ ...props }) => {
   return <div></div>;
 };
