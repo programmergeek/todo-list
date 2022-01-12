8585dbd590691587a9738c730e2080aacac088bb
programmergeek <programergeek@gmail.com>
2022-01-12 20:33:12 +0200

    remove functions from context

--- src/Components/ListContext.ts
+++ src/Components/ListContext.ts
@@ -1,24 +1,7 @@
 import React from "react";
 
 export const contextObj = {
-  data: {
-    list: [] as string[],
-  },
-  functions: {
-    addToList: (item: string, list: string[]) => {
-      list.push(item);
-    },
-    deleteFromList: (item: string, list: string[]) => {
-      list.filter((value) => value != item);
-    },
-    editList: (newItem: string, oldItem: string, list: string[]) => {
-      const i = list.indexOf(oldItem);
-      list[i] = newItem;
-    },
-    clearList: (list: string[]) => {
-      list.filter((value) => value.length > 0);
-    },
-  },
+  list: [] as string[],
 };
 
 export const ListContext = React.createContext(contextObj);
