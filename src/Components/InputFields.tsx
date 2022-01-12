acd9ff20e2dbf960cc84ae5970ad018e982d02ae
programmergeek <programergeek@gmail.com>
2022-01-12 21:41:14 +0200

    remove unneeded div in TextField

--- src/Components/InputFields.tsx
+++ src/Components/InputFields.tsx
@@ -7,13 +7,11 @@ type TextFieldProps = React.DetailedHTMLProps<
 
 export const TextField: React.FC<TextFieldProps> = ({ ...props }) => {
   return (
-    <div>
-      <input
-        type="text"
-        className="text-field px-5 lg:w-2/5 font-sans-serif md:w-1/2 w-4/5 rounded-full h-12 outline-none shadow-lg"
-        {...props}
-      />
-    </div>
+    <input
+      type="text"
+      className="text-field px-5 lg:w-2/5 font-sans-serif md:w-1/2 w-4/5 rounded-full h-12 outline-none shadow-lg"
+      {...props}
+    />
   );
 };
 
