0d199aacfe4f2226546c01098d7156238cb69f44
programmergeek <programergeek@gmail.com>
2022-01-12 14:53:42 +0200

    make entire check field component clickable

--- src/Components/CheckField.tsx
+++ src/Components/CheckField.tsx
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const CheckField: React.FC = () => {
+  const [isChecked, updateIsChecked] = useState(false);
+
   return (
-    <div className="check-field w-fit">
+    <div
+      className="check-field w-fit"
+      onChange={() => {
+        updateIsChecked(!isChecked);
+      }}
+    >
       <input
         type="checkbox"
         name="test"
         id="test"
         style={{ display: "none" }}
         className="self-center"
+        checked={isChecked}
+        onChange={() => null}
       />
       <label htmlFor="test" className="relative cursor-pointer font-sans-serif">
         This is Sparta
