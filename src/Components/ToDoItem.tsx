e1db0c6f246eb096fcf93d32e7ed68615c27d277
programmergeek <programergeek@gmail.com>
2022-01-14 17:57:25 +0200

    reorganise structure and style of to do record component

--- src/Components/ToDoItem.tsx
+++ src/Components/ToDoItem.tsx
@@ -12,16 +12,14 @@ export const ToDoItem: React.FC = () => {
 const ToDoRecord: React.FC = () => {
   return (
     <div className="flex justify-center">
-      <div className="flex lg:w-2/5 md:w-1/2 w-4/5 rounded-full bg-gray-50 shadow-md relative">
+      <div className="flex mr-2 lg:w-1/4 md:w-1/2 w-4/5 rounded-full bg-gray-50 shadow-lg">
         <div className="mt-3 ml-5">
           <CheckField />
         </div>
-        <div className="absolute left-3/4 top-1">
-          <EditButton onClick={() => null} />
-        </div>
-        <div className="absolute left-[87%] top-1">
-          <DeleteButton onClick={() => null} />
-        </div>
+      </div>
+      <div className="relative hidden sm:flex">
+        <EditButton onClick={() => null} />
+        <DeleteButton onClick={() => null} />
       </div>
     </div>
   );
