572a8f0ba95891693a4c8b578395f20d6c7a7bae
programmergeek <programergeek@gmail.com>
2022-01-14 23:36:36 +0200

    resize to do record width

--- src/Components/ToDoItem.tsx
+++ src/Components/ToDoItem.tsx
@@ -2,17 +2,13 @@ import React from "react";
 import { CheckField, OptionsButton } from ".";
 
 export const ToDoItem: React.FC = () => {
-  return (
-    <div className="mt-10">
-      <ToDoRecord />
-    </div>
-  );
+  return <ToDoRecord />;
 };
 
 const ToDoRecord: React.FC = () => {
   return (
     <div className="flex justify-center">
-      <div className="flex mr-2 lg:w-2/4 md:w-1/2 w-4/5 rounded-full bg-gray-50 shadow-lg">
+      <div className="flex mr-2 lg:w-4/12 md:w-1/2 w-4/5 rounded-full bg-gray-50 shadow-lg">
         <div className="mt-3 ml-5">
           <CheckField />
         </div>
