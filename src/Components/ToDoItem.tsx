ec35812c5e84da3e63faa6bd342fc9e10795fcd3
programmergeek <programergeek@gmail.com>
2022-01-15 13:36:18 +0200

    adjust options button positioning

--- src/Components/ToDoItem.tsx
+++ src/Components/ToDoItem.tsx
@@ -7,13 +7,13 @@ export const ToDoItem: React.FC = () => {
 
 const ToDoRecord: React.FC = () => {
   return (
-    <div className="flex justify-center">
+    <div className="flex justify-center relative">
       <div className="flex mr-2 lg:w-4/12 md:w-1/2 w-4/5 rounded-full bg-gray-50 shadow-lg">
         <div className="mt-3 ml-5">
           <CheckField />
         </div>
       </div>
-      <div className="relative hidden sm:flex">
+      <div className="absolute sm:relative right-[13vw] top-1.5 sm:flex sm:right-0 sm:top-0">
         <OptionsButton />
       </div>
     </div>
