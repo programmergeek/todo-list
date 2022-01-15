d1102605f876da60c54e415ce43929e2720dfd8e
programmergeek <programergeek@gmail.com>
2022-01-15 20:25:32 +0200

    context functions return new modified arrays instead of the same array but modified

--- src/Context/ContextFunctions.ts
+++ src/Context/ContextFunctions.ts
@@ -1,14 +1,11 @@
 export const addToList = (val: string, list: string[]) => {
-  if (val.length > 0) {
-    list.push(val);
-  }
-  return list;
+  return [...list, val];
 };
 
 export const deleteFromList = (val: string, list: string[]) => {
-  return list.filter((item) => item !== val);
+  return [...list.filter((item) => item !== val)];
 };
 
 export const clearList = (list: string[]) => {
-  return list.filter((item) => item.length > 0);
+  return [...list.filter((item) => item.length > 0)];
 };
