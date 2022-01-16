8a448bcb9b6a7fd17d3fc815128470462642ddbe
programmergeek <programergeek@gmail.com>
2022-01-16 15:48:37 +0200

    rework checkfield component

--- src/Components/CheckField.tsx
+++ src/Components/CheckField.tsx
@@ -1,28 +1,21 @@
 import React, { useState } from "react";
+import { Check } from "@styled-icons/bootstrap/Check";
 
 export const CheckField: React.FC<{ value: string }> = ({ ...props }) => {
   const [isChecked, updateIsChecked] = useState(false);
 
   return (
     <div
-      className="check-field w-fit"
-      onChange={() => {
+      className="relative check-field w-fit"
+      onClick={() => {
         updateIsChecked(!isChecked);
       }}
     >
-      <input
-        type="checkbox"
-        name="test"
-        id="test"
-        style={{ display: "none" }}
-        className="self-center"
-        checked={isChecked}
-        onChange={() => null}
-      />
-      <label htmlFor="test" className="relative cursor-pointer font-sans-serif">
-        {props.value}
-      </label>
-      <div className="checkmark"></div>
+      <div className="absolute left-2 top-1.5 rounded-full border-2 border-black w-5 h-5">
+        <Check style={{ display: isChecked ? "block" : "none" }} size={16} />
+      </div>
+
+      <p className="relative cursor-pointer font-sans-serif">{props.value}</p>
     </div>
   );
 };
